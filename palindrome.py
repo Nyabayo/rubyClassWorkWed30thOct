4e2600f54b17a059a1a2136389f400f33f9ff21c
nyabayo <ernestosindo9@gmail.com>
2024-10-30 10:04:21 +0300

    palindrome

---
+++ palindrome.py
@@ -0,0 +1,9 @@
+def is_palindrome(x):
+    return x == x[::-1]
+
+y = is_palindrome("dad")
+print(y)
+
+
+
+
---
+++ reverse_string_function.py
@@ -0,0 +1,12 @@
+
+#Create a function
+def group_one(n):
+
+  return n[::-1]
+
+#function call
+go = group_one("Group one")
+
+#output
+
+print(go)
