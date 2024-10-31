604e8236c76950b5d6e2b33c4dd5e28f0e1a306d
Mark <mrkcarlos576@gmail.com>
2024-10-31 08:18:29 +0300

    fibonacci

--- fibonacci.py
+++ fibonacci.py
@@ -1,1 +1,13 @@
+def fibonacci(x):
+    if x <= 0:
+        return []
+    elif x == 1:
+        return [0]
+    
+    fibonacci_sequence = [0, 1]
+    for _ in range(2, x):
+        fibonacci_sequence.append(fibonacci_sequence[-1] + fibonacci_sequence[-2])
+    return fibonacci_sequence[:x]
 
+y = fibonacci(5)
+print(y)
